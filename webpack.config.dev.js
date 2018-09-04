de045e0468d46ab1b21d1ce1566e8e6504fe1abf
YU-HSIN, CHEN <s63232@gmail.com>
2018-09-04 16:44:45 +0800

    first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+const webpackConfig = require('./webpack.config');
+
+module.exports = merge(webpackConfig, {
+    devtool: 'source-map',
+
+    output: {
+        pathinfo: true,
+        publicPath: '/',
+        filename: '[name].js',
+    },
+
+    // devServer: {
+    //     host: '0.0.0.0',
+    // },
+});
